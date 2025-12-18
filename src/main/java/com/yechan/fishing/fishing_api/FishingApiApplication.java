c59d0be44a3422e2bfc746242f5ffc31b0b89fc4
yechan <rladPcks980@gmail.com>
2025-12-18 21:54:15 +0900

    chore: init spring boot project

---
+++ src/main/java/com/yechan/fishing/fishing_api/FishingApiApplication.java
@@ -0,0 +1,13 @@
+package com.yechan.fishing.fishing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yechan/fishing/fishing_api/FishingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yechan.fishing.fishing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
