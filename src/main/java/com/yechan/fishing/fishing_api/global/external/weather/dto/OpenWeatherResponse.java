cee5991cc37d6ecb455e5658bb5634e7bda0ccdb
yechan <rladPcks980@gmail.com>
2025-12-27 16:33:48 +0900

    feat: OpenWeatherMap 응답 객체 확장

--- src/main/java/com/yechan/fishing/fishing_api/global/external/weather/dto/OpenWeatherResponse.java
+++ src/main/java/com/yechan/fishing/fishing_api/global/external/weather/dto/OpenWeatherResponse.java
@@ -1,13 +1,19 @@
 package com.yechan.fishing.fishing_api.global.external.weather.dto;
 
-
 import java.util.List;
 
 public class OpenWeatherResponse {
 
+    private long dt;
     private Main main;
     private List<Weather> weather;
+    private Wind wind;
+    private Clouds clouds;
     private Sys sys;
+
+    public long getDt() {
+        return dt;
+    }
 
     public Main getMain() {
         return main;
@@ -17,23 +23,69 @@ public class OpenWeatherResponse {
         return weather;
     }
 
+    public Wind getWind() {
+        return wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
     public Sys getSys() {
         return sys;
     }
 
+    /* ===== inner classes ===== */
+
     public static class Main {
         private double temp;
+        private double feels_like;
+        private int humidity;
 
         public double getTemp() {
             return temp;
+        }
+
+        public double getFeelsLike() {
+            return feels_like;
+        }
+
+        public int getHumidity() {
+            return humidity;
         }
     }
 
     public static class Weather {
         private String main;
+        private String description;
 
         public String getMain() {
             return main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    public static class Wind {
+        private double speed;
+        private int deg;
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public int getDeg() {
+            return deg;
+        }
+    }
+
+    public static class Clouds {
+        private int all;
+
+        public int getAll() {
+            return all;
         }
     }
 
@@ -49,5 +101,4 @@ public class OpenWeatherResponse {
             return sunset;
         }
     }
-
 }
